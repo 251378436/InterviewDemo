9b30fa2b888357da34ab39cf32561bcb0c523679
251378436 <251378436@qq.com>
2022-10-11 00:40:34 +1300

    Add unit tests

---
+++ API.Tests/Controllers/GuestsControllerTests.cs
@@ -0,0 +1,58 @@
+﻿using API.Contracts;
+using API.Contracts.Client;
+using API.Controllers;
+using API.Services;
+using AutoFixture;
+using AutoMapper;
+using FluentValidation;
+using FluentValidation.Results;
+using Microsoft.Extensions.Logging;
+using Moq;
+using Shouldly;
+
+namespace API.Tests.Controllers
+{
+    public class GuestsControllerTests
+    {
+        private Mock<ILogger<GuestsController>> _mockLogger;
+        private Mock<IGuestService> _mockGuestService;
+        private Mock<IMapper> _mockMapper;
+        private Mock<IValidator<Request>> _mockValidator;
+        private Fixture _fixture;
+        private GuestsController _sut;
+
+        public GuestsControllerTests()
+        {
+            _fixture = new Fixture();
+
+            _mockLogger = new Mock<ILogger<GuestsController>>();
+            _mockGuestService = new Mock<IGuestService>();
+            _mockMapper = new Mock<IMapper>();
+            _mockValidator = new Mock<IValidator<Request>>();
+
+            _sut = new GuestsController(_mockLogger.Object, _mockGuestService.Object,
+                            _mockMapper.Object, _mockValidator.Object);
+        }
+
+        [Fact]
+        public async Task Validate_ReturnOkResult_WhenInputIsValid_Test()
+        {
+            // Arrange
+            Request request = _fixture.Create<Request>();
+            var validationResult = _fixture.Create<ValidationResult>();
+            _mockMapper.Setup(x => x.Map<Guest>(It.IsAny<Request>())).Returns(_fixture.Create<Guest>());
+            _mockValidator.Setup(x => x.ValidateAsync(It.IsAny<Request>(), It.IsAny<CancellationToken>())).ReturnsAsync(validationResult);
+            _mockGuestService.Setup(x => x.ProcessGuestInfo(It.IsAny<Guest>())).Returns(Task.CompletedTask);
+
+            // Act
+            var response = await _sut.Post(request);
+
+            // Assert
+            var okResult = Assert.IsType<Response>(response);
+            response?.Message.ShouldBe("Saved");
+
+            _mockGuestService.VerifyAll();
+            _mockMapper.VerifyAll();
+        }
+    }
+}
---
+++ API.Tests/Services/GuestServiceTests.cs
@@ -0,0 +1,42 @@
+﻿using API.Contracts;
+using API.Repositories;
+using API.Services;
+using AutoFixture;
+using Microsoft.Extensions.Logging;
+using Moq;
+
+namespace API.Tests.Services
+{
+    public class GuestServiceTests
+    {
+        private Mock<ILogger<GuestService>> _mockLogger;
+        private Mock<IDataManager> _mockDataManager;
+        private Fixture _fixture;
+        private GuestService _sut;
+
+        public GuestServiceTests()
+        {
+            _fixture = new Fixture();
+
+            _mockLogger = new Mock<ILogger<GuestService>>();
+            _mockDataManager = new Mock<IDataManager>();
+
+            _sut = new GuestService(_mockDataManager.Object, _mockLogger.Object);
+        }
+
+        [Fact]
+        public async Task ProcessGuestInfo_Successful_Test()
+        {
+            // Arrange
+            _mockDataManager.Setup(x => x.SaveGuest(It.IsAny<Guest>())).Returns(Task.CompletedTask);
+
+            // Act
+            await _sut.ProcessGuestInfo(_fixture.Create<Guest>());
+
+            // Verify
+            _mockLogger
+                .VerifyLogging("Start processing guest information ......")
+                .VerifyLogging("Notify administrator......");
+        }
+    }
+}
